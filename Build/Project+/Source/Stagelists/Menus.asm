9a11dc845836e350934d2fb9bf4e2bdcb0e9de9a
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2023-12-21 15:07:50 -0600

    Add proposed 2023 stages
    Bugfix as well rofl

--- Build/Project+/Source/Stagelists/Menus.asm
+++ Build/Project+/Source/Stagelists/Menus.asm
@@ -30,12 +30,12 @@ string "stageinfo/"                     @ $80550B18 # This address might change 
 Set menus based on Code Menu Stagelist setting [Bird]
 ###############################################################
 * 20523400 00000002 # If 80523400 is equal to 2
-string "/menu2/br_menumain.pac"         @ $806FB248
-string "br_menumain_en.pac"             @ $817F62BC
-string "/menu2/br_selcharacter2.pac"    @ $806FF308
-string "br_selcharacter2_en.pac"        @ $817F634D
-string "/menu2/br_selmap.pac"           @ $806FF3F0
-string "br_selmap_en.pac"               @ $817F637C
+string "/menu2/sc_menumain.pac"         @ $806FB248
+string "sc_menumain_en.pac"             @ $817F62BC
+string "/menu2/sc_selcharacter2.pac"    @ $806FF308
+string "sc_selcharacter2_en.pac"        @ $817F634D
+string "/menu2/sc_selmap.pac"           @ $806FF3F0
+string "sc_selmap_en.pac"               @ $817F637C
 string "stageslot/"                   @ $80550B08 # This address might change and crash
 string "stageinfo/"                     @ $80550B18 # This address might change and crash
 * E0000000 80008000
