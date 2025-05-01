c7a76f04ea86109ac3311a72e8dac51bcd3a9ebd
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2025-05-01 14:17:46 -0500

    Start of 3.0.5 to 3.1.1 updating

--- Build/Project+/Source/Project+/StageTable.asm
+++
@@ -1,6 +0,0 @@
-.include Source/Stagelists/Default.asm
-.include Source/Stagelists/SATXSingles.asm
-.include Source/Stagelists/SATXDoubles.asm
-.include Source/Stagelists/2024Singles.asm
-.include Source/Stagelists/2023Singles.asm
-.include Source/Stagelists/PMBR.asm
